f5d1c43d3ba0842a5f9036f8fadbc6a54f94bd4f
eddymorfeo <edward.tejeda1987@gmail.com>
2026-02-18 16:29:16 -0300

    Inicio de proyecto

---
+++ components/src/components/login/login-form.tsx
@@ -0,0 +1,67 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <div className="bg-transparentrelative hidden lg:block">
+            <img
+              src="/logo_fiscalia.svg"
+              alt="Logo_Fiscalia"
+              className="h-50 w-full p-10"
+            />
+          </div>
+          <CardTitle className="pt-10">Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="user">Usuario</FieldLabel>
+                <Input
+                  id="user"
+                  type="text"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Contraseña</FieldLabel>
+                </div>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit">Login</Button>
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account? <a href="#">Sign up</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
